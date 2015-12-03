409b078617981786556c0de7a3e10ec04af7c9c5
KillDos <killdoslab@naver.com>
2015-12-04 00:20:57 +0900

    Update installNodejs.rb

--- oshApp/recipes/installNodejs.rb
+++ oshApp/recipes/installNodejs.rb
@@ -16,5 +16,10 @@ script "update new version" do
     n stable
     npm update npm -g
     npm install pm2 -g
+    wget http://people.centos.org/tru/devtools-2/devtools-2.repo -O /etc/yum.repos.d/devtools-2.repo
+    yum -y install devtoolset-2-gcc devtoolset-2-binutils devtoolset-2-gcc-c++
+    ln -s /opt/rh/devtoolset-2/root/usr/bin/* /usr/local/bin
+    hash -r
+    
     EOH
 end
