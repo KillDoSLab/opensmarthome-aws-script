bc0ccd9fed043cffbbd9f55865eff6b57a78e139
KillDos <killdoslab@naver.com>
2015-12-03 20:46:02 +0900

    Update installMongoDB.rb

--- oshApp/recipes/installMongoDB.rb
+++ oshApp/recipes/installMongoDB.rb
@@ -1,7 +1,6 @@
 node[:deploy].each do |app_name, deploy|
 
-  template "mongodb:set yum" do
-  	path value_fro_platform( 'default' => '/etc/yum.repos.d/mongodb.repo' )
+  template "/etc/yum.repos.d/mongodb.repo" do
   	source 'mongodb.repo.erb'
   	backup false
   	owner 'root'
@@ -9,30 +8,4 @@ node[:deploy].each do |app_name, deploy|
   	mode 0644
   end
   
-  script "install mongodb" do
-    interpreter "bash"
-    user "root"
-    cwd "#{deploy[:deploy_to]}/current"
-    code <<-EOH
-    yum -y install mongodb-org
-    EOH
-  end
-  
-  script "set chkconfig" do
-    interpreter "bash"
-    user "root"
-    cwd "#{deploy[:deploy_to]}/current"
-    code <<-EOH
-    chkconfig mongod on
-    EOH
-  end
-  
-  template "mongodb:set yum" do
-  	path value_fro_platform( 'default' => '/etc/mongod.conf' )
-  	source 'mongod.conf.erb'
-  	backup false
-  	owner 'root'
-  	group 'root'
-  	mode 0644
-  end
 end
