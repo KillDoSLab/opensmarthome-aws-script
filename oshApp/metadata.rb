575c5c7a16f5109f1801bd648ff68d1bdb20c743
KillDos Lab <killdoslab@naver.com>
2015-12-03 23:46:19 +0900

    "add file"

---
+++ oshApp/metadata.rb
@@ -0,0 +1,7 @@
+name        "OpenSmartHome"
+description "Installs and configures OSH"
+maintainer  "killdoslab"
+license     "LGPL 3.0"
+version     "0.0.0"
+
+recipe "oshApp::installMongoDB", "Installs DB"
---
+++ oshApp/recipes/installNodejs.rb
@@ -0,0 +1,16 @@
+package ['nodejs', 'npm'] do
+    action :install
+end
+
+script "update new version" do
+    interpreter "bash"
+    user "root"
+    cwd "/tmp"
+    code <<-EOH
+    npm cache clean -f
+    npm install -g n
+    n stable
+    npm update npm -g
+    EOH
+end
+
