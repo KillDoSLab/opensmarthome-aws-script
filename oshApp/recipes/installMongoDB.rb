8a4deea40773a29bfade0b3569df11b78645cbd6
KillDos <killdoslab@naver.com>
2015-12-03 21:10:43 +0900

    Update installMongoDB.rb

--- oshApp/recipes/installMongoDB.rb
+++ oshApp/recipes/installMongoDB.rb
@@ -1,11 +1,7 @@
-node[:deploy].each do |app_name, deploy|
-
-  template "/etc/yum.repos.d/mongodb.repo" do
+cookbook_file "/etc/yum.repos.d/mongodb.repo" do
   	source 'mongodb.repo.erb'
   	backup false
   	owner 'root'
   	group 'root'
   	mode 0644
-  end
-  
 end
